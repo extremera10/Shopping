ffa02b3208cdaf7ce4875151d3df685815283021
Extremera <extremera10extremera@gmail.com>
2024-01-03 01:20:49 +0100

    subiendo product-service

---
+++ product-service/src/main/java/com/store/serviceproduct/entity/Product.java
@@ -0,0 +1,41 @@
+package com.store.serviceproduct.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_products")
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "El nombre no debe ser vacío")
+    private String name;
+    private String description;
+    @Positive(message = "El stock debe ser mayor que cero")
+    private Double stock;
+    private Double price;
+    private String status;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    @NotNull(message = "La categoría no puede ser vacía")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Category category;
+}
